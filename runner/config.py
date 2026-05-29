4cc9b2ba0809ccfe7d5de87bacf99dcf555cb7ef
kevin.mogollon <kevin.mogollon@lim.upeu.edu.pe>
2026-05-29 16:50:27 -0500

    chore: imports, models (tagets and runners) runner up sync with api

--- runner/config.py
+++ runner/config.py
@@ -1,6 +1,24 @@
 import os
 
 
-CONTROL_PLANE_URL = os.getenv("CONTROL_PLANE_URL", "http://localhost:8000")
-RUNNER_TOKEN = os.getenv("RUNNER_TOKEN", "dev-runner-token")
-RUNNER_NAME = os.getenv("RUNNER_NAME", "L2Sec runner")
+CONTROL_PLANE_URL = os.getenv(
+    "CONTROL_PLANE_URL",
+    "http://localhost:8000",
+)
+
+RUNNER_TOKEN = os.getenv(
+    "RUNNER_TOKEN",
+    "dev-runner-token",
+)
+
+RUNNER_NAME = os.getenv(
+    "RUNNER_NAME",
+    "local-runner-01",
+)
+
+HEARTBEAT_INTERVAL_SECONDS = int(
+    os.getenv(
+        "HEARTBEAT_INTERVAL_SECONDS",
+        "10",
+    )
+)
